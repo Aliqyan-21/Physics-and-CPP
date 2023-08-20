19ee960f86040d6a0bdea718b5fec584fe165b33
aliqyan-21 <aliqyanabid21@gmail.com>
2023-08-20 11:52:13 +0530

    Kinematics - a motional branch of physics

--- 1_Kinematics/1_MotionInOnePlane(Circle)/run.sh
+++ 1_Kinematics/1_MotionInOnePlane(Circle)/run.sh
@@ -10,6 +10,7 @@ g++ code.cpp -o code
 # plot "Circle.dat" using 1:2 with lines title "x(t)"
 # replot "Circle.dat" using 1:3 with lines title "y(t)"
 
+# x0 = 0; y0 = 1
 # plot "Circle.dat" using 1:(atan2($3 - y0, $2 - x0)) with lines title "theta(t)", pi, -pi
 
 # plot "Circle.dat" using 1:4 title "v_x(t)" with lines
--- 1_Kinematics/2_SimplePendulum/run.sh
+++ 1_Kinematics/2_SimplePendulum/run.sh
@@ -9,7 +9,7 @@ g++ code.cpp -o code
 # gnuplot commands
 
 # plot "Pendulum.dat" u 1:2 w l t "x(t)"
-# plot "Pendulum.dat" u 1:3 w l t "y(t)"
+# replot "Pendulum.dat" u 1:3 w l t "y(t)"
 # plot "Pendulum.dat" u 1:4 w l t "v_x(t)"
 # replot "Pendulum.dat" u 1:5 w l t "v_y(t)"
 # plot "Pendulum.dat" u 1:6 w l t "theta(t)"
